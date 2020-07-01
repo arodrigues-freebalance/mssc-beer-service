17616d346955d598da6ce9cd122db4bbe7d6b5b7
arodrigues_freebalance <arodrigues@freebalance.com>
2020-07-01 10:01:15 +0100

    Fix CircleCI Integration Tests. Closes #31

--- src/test/java/akr/trainingmicro/msscbeerservice/MsscBeerServiceApplicationTests.java
+++ src/test/java/akr/trainingmicro/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -2,7 +2,9 @@ package akr.trainingmicro.msscbeerservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles(profiles = "default")
 @SpringBootTest
 class MsscBeerServiceApplicationTests {
 
