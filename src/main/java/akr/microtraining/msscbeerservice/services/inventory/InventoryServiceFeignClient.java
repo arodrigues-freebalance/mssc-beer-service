011d39ce242e2e0ec5d68744419a8604f88235b7
arodrigues_freebalance <arodrigues@freebalance.com>
2020-07-10 19:16:59 +0100

    Update Inventory Service to use OpenFeign. Closes #46

---
+++ src/main/java/akr/microtraining/msscbeerservice/services/inventory/InventoryServiceFeignClient.java
@@ -0,0 +1,19 @@
+package akr.microtraining.msscbeerservice.services.inventory;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import akr.microtraining.msscbeerservice.services.inventory.model.BeerInventoryDto;
+
+@FeignClient(name = "beer-inventory-service")
+public interface InventoryServiceFeignClient {
+
+    @RequestMapping(method = RequestMethod.GET, value = BeerInventoryServiceRestTemplateImpl.INVENTORY_PATH)
+    ResponseEntity<List<BeerInventoryDto>> getOnhandInventory(@PathVariable UUID beerId);
+}
