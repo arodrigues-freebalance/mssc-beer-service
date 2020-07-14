f78c7bbc3956a528d6381f49c0510e72b4c45757
arodrigues_freebalance <arodrigues@freebalance.com>
2020-07-14 14:03:22 +0100

    Configure HTTP Basic Auth for RESTTemplate Closes #55

--- src/main/java/akr/microtraining/msscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImpl.java
+++ src/main/java/akr/microtraining/msscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Profile;
@@ -18,7 +19,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Profile("!local-discovery")
 @Slf4j
-@ConfigurationProperties(prefix = "akr.brewery", ignoreUnknownFields = false)
+@ConfigurationProperties(prefix = "akr.brewery", ignoreUnknownFields = true)
 @Component
 public class BeerInventoryServiceRestTemplateImpl implements BeerInventoryService {
 
@@ -31,8 +32,12 @@ public class BeerInventoryServiceRestTemplateImpl implements BeerInventoryServic
         this.beerInventoryServiceHost = beerInventoryServiceHost;
     }
 
-    public BeerInventoryServiceRestTemplateImpl(RestTemplateBuilder restTemplateBuilder) {
-        this.restTemplate = restTemplateBuilder.build();
+    public BeerInventoryServiceRestTemplateImpl(RestTemplateBuilder restTemplateBuilder,
+            	@Value("${akr.brewery.inventory-user}") String inventoryUser,
+            		@Value("${akr.brewery.inventory-password}")String inventoryPassword) {
+			this.restTemplate = restTemplateBuilder
+			.basicAuthentication(inventoryUser, inventoryPassword)
+			.build();
     }
 
     @Override
